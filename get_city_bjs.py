977e602b89e4994abd8c7061522127f668a6b6d3
Max Marcussen <axemanscrums@cs.uchicago.edu>
2019-02-25 23:27:10 -0600

    found all the department codes for municipal PDs

--- get_city_bjs.py
+++ get_city_bjs.py
@@ -23,35 +23,39 @@ def get_national_data(csv_filename):
     driver.find_elements_by_xpath("//a[@id='atab1']")[0].click()
     time.sleep(3)
 
-cities = ['Pittsburgh, PA', 'Arlington' +\
-             ', TX', 'Corpus Christi, TX', 'Laredo, TX', 'Plano, TX', \
-             'Virginia Beach, VA', 'Tucson, AZ', 'Fresno, CA', 'San ' +\
-             'Francisco, CA', 'Denver, CO', 'Washington, DC', 'Jacksonville' +\
-             ', FL', 'Honolulu, HI', 'Indianapolis, IN', 'Louisville, KY', \
-             'Boston, MA', 'Baltimore, MD', 'Detroit, MI', 'Charlotte, NC', \
-             'Albuquerque, NM', 'Columbus, OH', 'Oklahoma City, OK', \
-             'Portland, OR', 'Memphis, TN', 'Nashville, TN', 'Austin, TX', \
-             'Fort Worth, TX', 'El Paso, TX', 'Seattle, WA', 'Milwaukee, WI', \
-             'Phoenix, AZ', 'Los Angeles, CA', 'San Diego, CA', 'San Jose, ' +\
-             'CA', 'Chicago, IL', 'Las Vegas, NV', 'New York City, NY', \
-             'Philadelphia, PA', 'Dallas, TX', 'Houston, TX', 'San Antonio, TX']
-
     department_code_dict = {"Alabama": ['"AL00201"'], "Alaska": ['"AK00101"'], 
-                            "Arizona": ['"AZ00705"', '"AZ00717"'], 
+                            "Arizona": ['"AZ00705"', '"AZ00717"', '"AZ01003"', 
+                            '"AZ00723"'], 
                             "California": ['"CA03001"', '"CA01502"', '"CA03702"', 
                             '"CA01941"', '"CA00109"', '"CA03313"', '"CA03404"', 
-                            '"CA03019"', '"CA03905"'], "Colorado": ['"CO00101"', 
+                            '"CA03019"', '"CA03905"', '"CA01005"', '"CA03800"', 
+                            '"CA01942"', '"CA03711"', '"CA04313"'], 
+                            "Colorado": ['"CO00101"', '"CODPD00"',
                             '"CO02101"'], "Florida": ['"FL01306"', '"FL04804"',
-                            '"FL02902"'], "Georgia": ['"GAAPD00"'], "Indiana": 
-                            ['"IN00201"'], "Kansas": ['"KS08703"'], "Kentucky": 
-                            ['"KY05602"'], "Louisiana": ['"LANPD00"'], "Minnesota": 
+                            '"FL02902"', '"FL01602"'], "Georgia": ['"GAAPD00"'], 
+                            "Indiana": ['"IN00201"', '"INIPD00"'], 
+                            "Kansas": ['"KS08703"'], "Kentucky": ['"KY05602"'], 
+                            "Louisiana": ['"LANPD00"'], "Minnesota": 
                             ['"MN02711"', '"MN06209"'], "Missouri": ['"MOSPD00"'],
-                            "North Carolina": ['"NC04102"', '"NC09201"'], 
+                            "North Carolina": ['"NC04102"', '"NC09201"', '"NC06001"'], 
                             "Nebraska": ['"NB05501"', '"NB02802"'], "New Jersey": 
-                            ['"NJ00906"', '"NJNPD00"'], "Nevada": ['"NV00203"'],
-                            "New York": ['"NY01401"'], "Ohio": ['"OHCIP00"', 
-                            '"OHCLP00"', '"OH04807"'], "Oklahoma": ['"OK07205"'],
-                            "Pennsylvania": ['""'], "": ['""'],}
+                            ['"NJ00906"', '"NJNPD00"'], "Nevada": ['"NV00203"', 
+                            '"NV00201"'],
+                            "New York": ['"NY01401"', '"NY03030"'], 
+                            "Ohio": ['"OHCIP00"', 
+                            '"OHCLP00"', '"OH04807"', '"OHCOP00"'], 
+                            "Oklahoma": ['"OK07205"', '"OK05506"'],
+                            "Pennsylvania": ['"PAPPD00"', '"PAPEP00"'], 
+                            "Texas": ['"TX22001"', '"TXDPD00"', '"TXHPD00"',
+                            '"TX17802"', '"TX24001"', '"TX04306"', '"TXSPD00"',
+                            '"TX22701"', '"TX22012"', '"TX07102"'], "Virginia": 
+                            ['"VA12800"'], "District of Columbia": ['"DCMPD00"'],
+                            "Hawaii": ['"HI00200"'], "Massachusetts": ['"MA01301"'],
+                            "Maryland": ['"MDBPD00"'], "Michigan": ['"MI82349"'],
+                            "New Mexico": ['"NM00101"'], "Oregon": ['"OR02602"'],
+                            "Tennessee": ['"TNMPD00"', '"TN01901"'], 
+                            "Washington": ['"WASPD00"'], "Wisconsin": ['"WIMPD00"'],
+                            "Illinois": ['"ILCPD00"'],}
 
     grab_from_site = ['Violent Crime Index', 'Murder and Non-Negligent' +\
     ' Manslaughter', 'Forcible Rape', 'Robbery', 'Aggravated Assault', 'Property' +\

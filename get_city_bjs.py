6eac94412052da881448e19d05bc52dd7135032b
Samantha Stagg <staggsamantha@cs.uchicago.edu>
2019-02-26 19:23:47 -0600

    this took 7 hours

--- get_city_bjs.py
+++ get_city_bjs.py
@@ -8,14 +8,14 @@ import time
 def get_national_data(csv_filename):
     '''
     '''
-    head = ['Year', 'State', 'Department', 'Violent Crime'+\
+    head = ['Year', 'City', 'Violent Crime'+\
             ' Total', 'Murder and Nonnegligent Manslaughter', 'Rape', \
             'Robbery', 'Aggravated Assault', 'Property Crime Total', \
             'Burglary', 'Larceny-Theft', 'Motor Vehicle Theft']
 
-    with open(csv_filename, mode='a') as national_data:
-        national_writer = csv.writer(national_data, delimiter=',', quoting=csv.QUOTE_ALL)
-        #national_writer.writerow(head)
+    with open(csv_filename, mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        csv_writer.writerow(head)
 
     driver = webdriver.Firefox()
     driver.get('https://www.bjs.gov/index.cfm?ty=datool&surl=/arrests/index.cfm#')
@@ -34,7 +34,7 @@ def get_national_data(csv_filename):
                             '"AZ00723"'], 
                             "California": ['"CA03001"', '"CA01502"', '"CA03702"', 
                             '"CA01941"', '"CA00109"', '"CA03313"', '"CA03404"', 
-                            '"CA03019"', '"CA03905"', '"CA01005"', '"CA03800"', 
+                            '"CA03019"', '"CA03905"', '"CA01005"', '"CA03801"', 
                             '"CA01942"', '"CA03711"', '"CA04313"'], 
                             "Colorado": ['"CO00101"', '"CODPD00"',
                             '"CO02101"'], "Florida": ['"FL01306"', '"FL04804"',
@@ -62,6 +62,36 @@ def get_national_data(csv_filename):
                             "Tennessee": ['"TNMPD00"', '"TN01901"'], 
                             "Washington": ['"WASPD00"'], "Wisconsin": ['"WIMPD00"'],
                             "Illinois": ['"ILCPD00"'],}
+    state_keys = {'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', \
+                 'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO', \
+                 'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL', \
+                 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID', 'Illinois': \
+                 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', \
+                 'Kentucky': 'KY', 'Louisiana': 'LA', 'Maine': 'ME', \
+                 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', \
+                 'Minnesota': 'MN', 'Mississippi': 'MS', 'Missouri': 'MO', \
+                 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', \
+                 'New Hampshire': 'NH', 'New Jersey': 'NJ', 'New Mexico': \
+                 'NM', 'New York': 'NY', 'North Carolina': 'NC', \
+                 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR', \
+                 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina':\
+                  'SC', 'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': \
+                 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA', \
+                 'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI',\
+                  'Wyoming': 'WY', 'District of Columbia': 'DC'}
+    weird_names = {'TX22012': 'Fort Worth, TX', 'CA03702': \
+                    'Chula Vista, CA', 'CA01941': 'Long Beach, CA', \
+                    'CA03019': 'Santa Ana, CA', 'CO02101': \
+                    'Colorado Springs, CO', 'IN00201': 'Fort Wayne, IN', \
+                    'LANPD00': 'New Orleans, LA', 'MN06209': 'St. Paul, MN', \
+                    'MOSPD00': 'St. Louis, MO', 'NJ00906': 'Jersey City, NJ', \
+                    'TX17802': 'Corpus Christi, TX', 'VA12800': \
+                    'Virginia Beach, VA', 'CA03801': 'San Francisco, CA', \
+                    'OK05506': 'Oklahoma City, OK', 'TX07102': 'El Paso, TX', \
+                    'CA01942': 'Los Angeles, CA', 'CA03711': 'San Diego, CA', \
+                    'CA04313': 'San Jose, CA', 'NV00201': 'Las Vegas, NV', \
+                    'NY03030': 'New York City, NY', 'TXSPD00': \
+                    'San Antonio, TX'}
 
     grab_from_site = ['Violent Crime Index', 'Murder and Non-Negligent' +\
     ' Manslaughter', 'Forcible Rape', 'Robbery', 'Aggravated Assault', 'Property' +\
@@ -72,18 +102,19 @@ def get_national_data(csv_filename):
         state = driver.find_elements_by_xpath(
             "//option[@value="+str(option)+"]")[0].text 
         if state in department_code_dict:
-            print(state)
             time.sleep(5)
             for department_code in department_code_dict[state]:
-                print(department_code)
                 button = driver.find_elements_by_xpath(
                     "//option[@value="+department_code+"]")
                 if len(button) != 0:
                     button[0].click()
+                if department_code not in weird_names:
+                    city = button[0].text.split()[0] + ', ' + state_keys[state]
+                else:
+                    city = weird_names[department_code]
                 department = button[0].text
-                print(department)
-                for year in range(2000, 2015):
-                    data_list = [str(year), state, department]
+                for year in range(2001, 2015):
+                    data_list = [str(year), city]
                     driver.find_elements_by_xpath(
                         "//option[@value="+str(year)+"]")[0].click()    
                     driver.find_elements_by_xpath(
@@ -98,6 +129,4 @@ def get_national_data(csv_filename):
                         data_writer = csv.writer(data, 
                             delimiter=',', quoting = csv.QUOTE_ALL)
                         data_writer.writerow(data_list)
-
-
-get_national_data('bjs_city.csv')
+    driver.close()

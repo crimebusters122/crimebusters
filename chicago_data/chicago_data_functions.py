ad57c3a79ba6514d1bff40019f42572a0a9d2d58
Liam Puknys <lpuknys@cs.uchicago.edu>
2019-03-10 23:04:54 -0500

    map function appears to be working

--- chicago_data/chicago_data_functions.py
+++ chicago_data/chicago_data_functions.py
@@ -1,106 +1,55 @@
 import numpy as np 
 import pandas as pd 
-import matplotlib.pyplot as plt
-import geopandas as gpd
-from bokeh.plotting import figure, save
-from bokeh.models import ColumnDataSource, HoverTool
 import folium
+import folium.plugins
 
 
 
 
-def folium_map_chicago():
+def map_chicago_crime(
+	quick = False, 
+	num = 1000, 
+	file = 'chicago_crime_2018.csv',
+	prim_type = False,
+	):
     '''
     '''
 
     chicago_coordinates = (41.881832, -87.623177)
-    data_set = pd.read_csv('chicago_crime_2018.csv')
-
-    max_records = 1000
+    data_set = pd.read_csv(file)
 
     mp = folium.Map(location = chicago_coordinates, zoom_start = 10)
 
-    geographical_crimes = data_set.dropna()
+    if prim_type:
+    	data_set = data_set.loc[lambda data_set: \
+    	data_set['Primary Type'] == prim_type]
 
-    for crime in geographical_crimes[0:max_records].iterrows():
-        primary_type = crime[1]['Primary Type']
-        date = crime[1]['Date']
-        folium.Marker(
-            location = [crime[1]['Latitude'], crime[1]['Longitude']], 
-            tooltip = '<b>' + "Type of crime: " + primary_type + \
-                "--- Date: " + date
-        ).add_to(mp)
+    to_sample = min(num, len(data_set.index))
+
+    geographical_crimes = data_set.dropna().sample(to_sample)
+
+    if quick or num > 5000:
+    	locations = []
+    	for crime in geographical_crimes.iterrows():
+    		locations.append([crime[1]['Latitude'], crime[1]['Longitude']])
+
+    	marker_cluster = folium.plugins.FastMarkerCluster(locations)
+    else:
+        marker_cluster = folium.plugins.MarkerCluster()
+
+        for crime in geographical_crimes.iterrows():
+            primary_type = crime[1]['Primary Type']
+            date = crime[1]['Date']
+            marker_cluster.add_child(folium.Marker(
+                location = [crime[1]['Latitude'], crime[1]['Longitude']], 
+                tooltip = '<b>' + "Type of crime: " + primary_type + \
+                    "--- Date: " + date
+            ))
+
+    mp.add_child(marker_cluster)
+
     return mp
 
 
 
 
-
-
-'''
-
-def linear_regression(x,y):
-    '''
-    #Makes a linear regression table
-    '''
-
-    model = sm.OLS(y, x).fit()
-
-    return model.summary()
-
-
-
-def map_of_chicago_crime(c_type = 'every', year_min = 2001, year_max = 2018,\
-    arrest = 'irrelelevant'):
-    '''
-    #Makes a map of the density of a crime in Chicago.  Clicking on a dot/
-        #neighborhood will give you more indepth information.
-    '''
-
-    mp = gdp.read_file('Neighborhoods_2012b.shp')
-
-    data_set = pd.read_csv('chicago_crime_2018.csv', index = "ID")
-
-    x_coords = data_set.as_matrix(columns = data_set.columns('Longitude'))
-
-    y_coords = data_set.as_matrix(columns = data_set.columns('Latitude'))
-
-    mp.circle(x = x_coords, y = y_coords, size = 1, color = "red")
-
-
-    return save(obj = mp, filename = 'crime_map')
-
-
-def chicago_map():
-    '''
-    '''
-
-    file_path = "Neighborhoods_2012b.shp"
-    geo_data = gpd.read_file(file_path)
-
-    geo_data['x'] = geo_data.apply(get_poly_coords, coord = 'x', axis = 1)
-    geo_data['y'] = geo_data.apply(get_poly_coords, coord = 'y', axis = 1)
-
-    return(geo_data)
-
-    #cols_to_drop = ['PRI_NEIGH', 'SEC_NEIGH', 'geometry']
-    #usable_geo_data = pd.DataFrame(geo_data.drop(cols_to_drop, axis = 1))
-    #mpsource = ColumnDataSource(usable_geo_data)
-    #mp = figure(title = 'Chicago')
-    #mp.multiline('x', 'y', source = mpsource, color = 'red', line_width = 1)
-
-    #return save(obj = mp, filename = 'crime_map')
-
-
-def get_poly_coords(row, coord, geometry = 'geometry'):
-    '''
-    #Returns the x or y coordinates of the edges of a polygon
-    '''
-
-    boundary = row[geometry].boundary
-
-    if coord == "x":
-        return list(boundary.coords.xy[0])
-    if coord == "y":
-        return list(boundary.coord.xy[1])
-'''

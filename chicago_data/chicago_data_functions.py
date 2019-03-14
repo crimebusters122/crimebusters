5ac3d098103de216b2e9f7c908b637dcb7abe153
Liam Puknys <lpuknys@cs.uchicago.edu>
2019-03-14 12:11:37 -0500

    Everything is awesome on this front, time to tend to the others!

--- chicago_data/chicago_data_functions.py
+++ chicago_data/chicago_data_functions.py
@@ -5,55 +5,6 @@ import folium.plugins
 import sqlite3
 
 
-
-
-def map_chicago_crime(
-    quick = False, 
-    num = 1000, 
-    file = 'chicago_crime_2018.csv',
-    prim_type = False,
-    ):
-    '''
-    '''
-
-    chicago_coordinates = (41.881832, -87.623177)
-    data_set = pd.read_csv(file)
-
-    mp = folium.Map(location = chicago_coordinates, zoom_start = 10)
-
-    if prim_type:
-        data_set = data_set.loc[lambda data_set: \
-        data_set['Primary Type'] == prim_type]
-
-    to_sample = min(num, len(data_set.index))
-
-    geographical_crimes = data_set.dropna().sample(to_sample)
-
-    if quick or num > 5000:
-        locations = []
-        for crime in geographical_crimes.iterrows():
-            locations.append([crime[1]['Latitude'], crime[1]['Longitude']])
-
-        marker_cluster = folium.plugins.FastMarkerCluster(locations)
-    else:
-        marker_cluster = folium.plugins.MarkerCluster()
-
-        for crime in geographical_crimes.iterrows():
-            primary_type = crime[1]['Primary Type']
-            date = crime[1]['Date']
-            marker_cluster.add_child(folium.Marker(
-                location = [crime[1]['Latitude'], crime[1]['Longitude']], 
-                tooltip = '<b>' + "Type of crime: " + primary_type + \
-                    "--- Date: " + date
-            ))
-
-    mp.add_child(marker_cluster)
-
-    return mp
-
-
-
-
 def map_chicago_crime_db(
     quick = False, 
     num = 1000, 
@@ -69,22 +20,26 @@ def map_chicago_crime_db(
     db_connection = sqlite3.connect(data_base_path)
     db_cursor = db_connection.cursor()
 
-
     mp = folium.Map(location = chicago_coordinates, zoom_start = 10)
 
-    query = construct_query('crime', quick, num, year, prim_type)
+    query = construct_query('crime', quick, year, prim_type)
 
     print(query)
 
-    r = db_cursor.execute(query)
+    r = db_cursor.execute(query, [num])
+
+    data = r.fetchall()
+    print(data[0])
 
     if quick:
-        marker_cluster = folium.plugins.FastMarkerCluster(r.fetchall())
+        usable_data = []
+        for loc in data:
+            usable_data.append([float(loc[0]), float(loc[1])])
+        print(usable_data[0][0:5], usable_data[1][0:5])
+        marker_cluster = folium.plugins.FastMarkerCluster(usable_data)
+
     else:
         marker_cluster = folium.plugins.MarkerCluster()
-
-        data = r.fetchall()
-        print(data[0])
 
         for crime in range(len(data)):
 
@@ -103,7 +58,7 @@ def map_chicago_crime_db(
     return mp
 
 
-def construct_query(table, quick, num, year, prim_type):
+def construct_query(table, quick, year, prim_type):
     '''
     Constructs a query to get the information we want out of the database/
     '''
@@ -111,16 +66,20 @@ def construct_query(table, quick, num, year, prim_type):
     query = "SELECT Latitude, Longitude"
 
     if not quick:
-        query += ", [Primary.Type], Date"
+        query += ", \"Primary.Type\", Date"
 
-    query += " FROM " + table + " WHERE Latitude != \"NA\" AND " 
-    query += "rowid IN (SELECT rowid FROM " + table + " ORDER BY RANDOM()" 
-    query += " LIMIT " + str(num) + ")"
+    query += " FROM " + table + " WHERE"
+
+    query += " rowid IN (SELECT rowid FROM " + table
 
     if year:
-        query += " AND Year >= " + str(year[0]) +" AND Year <= " + str(year[1])
-    if prim_type:
-        query += " AND [Primary.] = " + prim_type +";"
+        query += " WHERE Year = \'" + str(year) +"\'"
+    if prim_type and not year:
+        query += " WHERE \"Primary.Type\" = \'" + prim_type + "\'"
+    if prim_type and year:
+        query += " AND \"Primary.Type\" = \'" + prim_type + "\'"
+
+    query += " ORDER BY RANDOM()" + " LIMIT ?);"
 
     return query
 

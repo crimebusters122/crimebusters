de836f1c770fc78885f537e9faf654c5397d1edd
Max Marcussen <axemanscrums@cs.uchicago.edu>
2019-03-15 14:50:13 -0500

    frustrated

--- cbsite/data_chicago/views.py
+++ cbsite/data_chicago/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 
+import folium
+
 from . import models
 from .models import LocationType1
 from .forms import InputForm
 import sqlite3
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
+from django.shortcuts import render_to_response
 from . import chicago_data_functions
 
 def stuff(request):
@@ -18,14 +21,14 @@ def stuff(request):
             crumb = form.cleaned_data['crimefield']
             tooltips = form.cleaned_data['tooltipfield']
             datapoints = form.cleaned_data['datapointfield']
-            load_map(year, crumb, tooltips, datapoints)
+            load_map(request, year, crumb, tooltips, datapoints)
             return HttpResponseRedirect('')
     else:
         form = InputForm()
 
     return render(request, 'data_chicago/loctype.html', {'form': form})
 
-def load_map(year, crumb, tooltips, datapoints):
+def load_map(request, year, crumb, tooltips, datapoints):
     if tooltips == "Yes":
         tooltips = False
     elif tooltips == "No":
@@ -37,4 +40,6 @@ def load_map(year, crumb, tooltips, datapoints):
             year = year,
             prim_type = crumb
             )
-        print(marp)
+        meep = marp._repr_html_()
+        print(meep)
+        return render(request, "data_chicago/marp.html", {"meep": meep})
